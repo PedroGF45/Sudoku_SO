519accf5833c1ed45da2eb717c5a23b1b0518ff1
Cláudio Pinto <128715792+StunFlyy@users.noreply.github.com>
2024-10-25 10:06:44 +0100

    Refactor
    Refactor do codigo do server.c (mais legivel)

--- Server/src/server.c
+++ Server/src/server.c
@@ -7,6 +7,11 @@
 #include "../config/config.h"
 #include "jogos.h"
 
+void inicializaSocket(struct sockaddr_in *serv_addr, int *sockfd, ServerConfig config);
+void enviarTabuleiro(int socket, Jogo *jogo);
+int receberLinhaDoCliente(int socket, char *buffer);
+int validarLinha(char *buffer);
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Erro: Faltam argumentos de configuracao!\n");
@@ -20,37 +25,16 @@ int main(int argc, char *argv[]) {
     int sockfd, newSockfd;
     struct sockaddr_in serv_addr;
 
-    // Criar socket
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        err_dump(config.logPath, 0, 0, " : can't open stream socket", EVENT_CONNECTION_SERVER_ERROR);
-    }
-
-    // Limpar a estrutura do socket
-    memset((char *) &serv_addr, 0, sizeof(serv_addr));
-
-    // Preencher a estrutura do socket
-    serv_addr.sin_family = AF_INET; // enderecos de internet DARPA
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY); // aceitar ligacoes de qualquer endereco
-    serv_addr.sin_port = htons(config.serverPort); // porta do servidor
-
-    // Associar o socket a um endereco qualquer
-    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        err_dump(config.logPath, 0, 0, " : can't bind local address", EVENT_CONNECTION_SERVER_ERROR);
-    }
-    
-    // Ouvir o socket
-    listen(sockfd, 1);
+    // Inicializar o socket
+    inicializaSocket(&serv_addr, &sockfd, config);
 
     char buffer[256];
-    char logMessage[BUFFER_SIZE];
 
     for (;;) {
-
-        // Aceitar ligacao
+        // Aceitar ligação
         if ((newSockfd = accept(sockfd, (struct sockaddr *) 0, (int *) 0)) < 0)
-            err_dump(config.logPath, 0, 0, " : accept error", EVENT_CONNECTION_SERVER_ERROR );
+            err_dump(config.logPath, 0, 0, " : accept error", EVENT_CONNECTION_SERVER_ERROR);
 
-        // Print conexao estabelecida
         printf("Conexao estabelecida com um cliente\n");
 
         // Carregar o jogo do ficheiro JSON
@@ -58,27 +42,8 @@ int main(int argc, char *argv[]) {
         int idJogador = 1; // Pode ser parametrizado conforme necessidade
         Jogo jogo = carregaJogo(config, idJogo, idJogador);
 
-        // Enviar tabuleiro ao cliente em formato JSON
-        JSON_Value *root_value = json_value_init_object();
-        JSON_Object *root_object = json_value_get_object(root_value);
-        json_object_set_number(root_object, "id", jogo.id);
-        JSON_Value *tabuleiro_value = json_value_init_array();
-        JSON_Array *tabuleiro_array = json_value_get_array(tabuleiro_value);
-
-        for (int i = 0; i < 9; i++) {
-            JSON_Value *linha_value = json_value_init_array();
-            JSON_Array *linha_array = json_value_get_array(linha_value);
-            for (int j = 0; j < 9; j++) {
-                json_array_append_number(linha_array, jogo.tabuleiro[i][j]);
-            }
-            json_array_append_value(tabuleiro_array, linha_value);
-        }
-
-        json_object_set_value(root_object, "tabuleiro", tabuleiro_value);
-        char *serialized_string = json_serialize_to_string(root_value);
-        send(newSockfd, serialized_string, strlen(serialized_string), 0);
-        json_free_serialized_string(serialized_string);
-        json_value_free(root_value);
+        // Enviar tabuleiro ao cliente
+        enviarTabuleiro(newSockfd, &jogo);
 
         // Receber e validar as linhas do cliente
         for (int i = 0; i < 9; i++) {
@@ -87,45 +52,26 @@ int main(int argc, char *argv[]) {
                 memset(buffer, 0, sizeof(buffer));
 
                 // Receber linha do cliente
-                int n = recv(newSockfd, buffer, sizeof(buffer), 0);
-                if (n <= 0) {
+                if (receberLinhaDoCliente(newSockfd, buffer) <= 0) {
                     printf("Conexao terminada com o cliente\n");
                     close(newSockfd);
                     break;
                 }
 
                 printf("Verificando linha %d...\n", i + 1);
-                printf("Linha inserida: ");
-                for (int j = 0; j < 9; j++) {
-                    printf("%c ", buffer[j]);
-                }
-                printf("\n");
 
-                // Verificar se a linha recebida tem exatamente 9 digitos
-                if (strlen(buffer) != 9) {
-                    strcpy(buffer, "incorreto: A linha deve ter exatamente 9 digitos");
+                // Validar linha
+                if (validarLinha(buffer) != 0) {
+                    strcpy(buffer, "incorreto: A linha deve ter exatamente 9 digitos e ser numerica");
                     send(newSockfd, buffer, strlen(buffer), 0);
-                    // Enviar o tabuleiro mesmo se a linha estiver incorreta
-                    goto enviar_tabuleiro;
+                    enviarTabuleiro(newSockfd, &jogo);
+                    continue;
                 }
 
                 // Converte a linha recebida em valores inteiros
                 int linhaInserida[9];
-                int inputValido = 1;
                 for (int j = 0; j < 9; j++) {
-                    if (buffer[j] < '0' || buffer[j] > '9') {
-                        inputValido = 0;
-                        break;
-                    }
                     linhaInserida[j] = buffer[j] - '0';
-                }
-
-                // Se a linha contiver caracteres inválidos
-                if (!inputValido) {
-                    strcpy(buffer, "incorreto: A linha contem caracteres nao numericos");
-                    send(newSockfd, buffer, strlen(buffer), 0);
-                    // Enviar o tabuleiro mesmo se a linha estiver incorreta
-                    goto enviar_tabuleiro;
                 }
 
                 // Verificar a linha recebida com a função verificaLinha
@@ -137,33 +83,8 @@ int main(int argc, char *argv[]) {
                 } else {
                     strcpy(buffer, "incorreto: Valores incorretos na linha, tente novamente");
                 }
-
                 send(newSockfd, buffer, strlen(buffer), 0);
-
-                // Enviar o tabuleiro após a interação
-                enviar_tabuleiro:
-
-                // Enviar o tabuleiro atualizado ao cliente
-                JSON_Value *updated_root_value = json_value_init_object();
-                JSON_Object *updated_root_object = json_value_get_object(updated_root_value);
-                json_object_set_number(updated_root_object, "id", jogo.id);
-                JSON_Value *updated_tabuleiro_value = json_value_init_array();
-                JSON_Array *updated_tabuleiro_array = json_value_get_array(updated_tabuleiro_value);
-
-                for (int k = 0; k < 9; k++) {
-                    JSON_Value *linha_value = json_value_init_array();
-                    JSON_Array *linha_array = json_value_get_array(linha_value);
-                    for (int j = 0; j < 9; j++) {
-                        json_array_append_number(linha_array, jogo.tabuleiro[k][j]);
-                    }
-                    json_array_append_value(updated_tabuleiro_array, linha_value);
-                }
-
-                json_object_set_value(updated_root_object, "tabuleiro", updated_tabuleiro_value);
-                char *updated_serialized_string = json_serialize_to_string(updated_root_value);
-                send(newSockfd, updated_serialized_string, strlen(updated_serialized_string), 0);
-                json_free_serialized_string(updated_serialized_string);
-                json_value_free(updated_root_value);
+                enviarTabuleiro(newSockfd, &jogo);
             }
         }
 
@@ -172,5 +93,68 @@ int main(int argc, char *argv[]) {
     }
 
     close(sockfd);
+    return 0;
+}
+
+void inicializaSocket(struct sockaddr_in *serv_addr, int *sockfd, ServerConfig config) {
+    // Criar socket
+    if ((*sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        err_dump(config.logPath, 0, 0, " : can't open stream socket", EVENT_CONNECTION_SERVER_ERROR);
+    }
+
+    // Limpar a estrutura do socket
+    memset((char *) serv_addr, 0, sizeof(*serv_addr));
+
+    // Preencher a estrutura do socket
+    serv_addr->sin_family = AF_INET;
+    serv_addr->sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr->sin_port = htons(config.serverPort);
+
+    // Associar o socket a um endereco qualquer
+    if (bind(*sockfd, (struct sockaddr *) serv_addr, sizeof(*serv_addr)) < 0) {
+        err_dump(config.logPath, 0, 0, " : can't bind local address", EVENT_CONNECTION_SERVER_ERROR);
+    }
+
+    // Ouvir o socket
+    listen(*sockfd, 1);
+}
+
+void enviarTabuleiro(int socket, Jogo *jogo) {
+    // Enviar tabuleiro ao cliente em formato JSON
+    JSON_Value *root_value = json_value_init_object();
+    JSON_Object *root_object = json_value_get_object(root_value);
+    json_object_set_number(root_object, "id", jogo->id);
+    JSON_Value *tabuleiro_value = json_value_init_array();
+    JSON_Array *tabuleiro_array = json_value_get_array(tabuleiro_value);
+
+    for (int i = 0; i < 9; i++) {
+        JSON_Value *linha_value = json_value_init_array();
+        JSON_Array *linha_array = json_value_get_array(linha_value);
+        for (int j = 0; j < 9; j++) {
+            json_array_append_number(linha_array, jogo->tabuleiro[i][j]);
+        }
+        json_array_append_value(tabuleiro_array, linha_value);
+    }
+
+    json_object_set_value(root_object, "tabuleiro", tabuleiro_value);
+    char *serialized_string = json_serialize_to_string(root_value);
+    send(socket, serialized_string, strlen(serialized_string), 0);
+    json_free_serialized_string(serialized_string);
+    json_value_free(root_value);
+}
+
+int receberLinhaDoCliente(int socket, char *buffer) {
+    return recv(socket, buffer, 9, 0);
+}
+
+int validarLinha(char *buffer) {
+    if (strlen(buffer) != 9) {
+        return -1;
+    }
+    for (int i = 0; i < 9; i++) {
+        if (buffer[i] < '0' || buffer[i] > '9') {
+            return -1;
+        }
+    }
     return 0;
 }
